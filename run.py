5a201d6acd57c4c97490e4ff0b2ab957e3782dc7
Yew Chong <83965035+yewey2@users.noreply.github.com>
2023-07-20 02:19:27 +0800

    debug

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from app import create_app, socketio
 
 app = create_app()
-socketio.run(app, host="0.0.0.0", port=8080, allow_unsafe_werkzeug=True)
-# if __name__ == '__main__':
-#     # socketio.run(app, allow_unsafe_werkzeug=True)
-#     socketio.run(app)
+# socketio.run(app, host="0.0.0.0", port=8080, allow_unsafe_werkzeug=True)
+if __name__ == '__main__':
+    # socketio.run(app, allow_unsafe_werkzeug=True)
+    socketio.run(app)
