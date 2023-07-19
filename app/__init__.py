3ae422d716ddbc4d7b605e88c58e71f9a2f01fe8
Yew Chong <83965035+yewey2@users.noreply.github.com>
2023-07-20 02:09:33 +0800

    debug

--- app/__init__.py
+++ app/__init__.py
@@ -6,10 +6,9 @@ from .routes import main
 
 def create_app():
     app = Flask(__name__, static_url_path='/static')
-    app.config["DEBUG"] = True
+    app.config["DEBUG"] = False
     app.config["SECRET_KEY"] = "secretkey"
 
-    # socketio = SocketIO(app)
     app.register_blueprint(main)
 
     socketio.init_app(app)
