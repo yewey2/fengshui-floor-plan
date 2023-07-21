1608185ed86345dad41491715daa48baf431cb63
Yew Chong <83965035+yewey2@users.noreply.github.com>
2023-07-21 23:22:00 +0800

    antialiasing of picture

--- app/image_overlay.py
+++ app/image_overlay.py
@@ -2,20 +2,28 @@ from PIL import Image, ImageDraw
 import io
 
 
-def overlay(img_path, angle=0, center=(0,0)):
-    center = tuple(int(i) for i in center)
+def overlay(img_byte = None, img_path = None, angle=0, center=(0,0), factor=10):
+    #factor is rescale factor
+    center = tuple(int(i)*factor for i in center)
     # center = (100,200)
-    # img1 = Image.open(r'{}'.format(img_path)).convert('RGBA')
     # img1 = Image.Image.frombytes(data=img_path).convert('RGBA')
-    img1 = Image.open(io.BytesIO(img_path)).convert('RGBA')
+    if img_path:
+        img1 = Image.open(r'{}'.format(img_path)).convert('RGBA')
+    elif img_byte:
+        img1 = Image.open(io.BytesIO(img_byte)).convert('RGBA')
+    else:
+        raise Exception
+    
+    width, height = img1.size
+    img1 = img1.resize((factor*width, factor*height), resample=Image.Resampling.LANCZOS)
     # img1.show()
     img2 = Image.open(r'app/static/octa.png').convert('RGBA')
-    width, height = img1.size
     # img2.show()
-    img2 = img2.resize((max(width, height),max(width, height))).rotate(angle)
+    img2 = img2.resize((factor*max(width, height), factor*max(width, height))).rotate(angle)
     length = img2.size[0]
     img1.paste(img2, (center[0]-length//2, center[1]-length//2), img2)
 
+    img1 = img1.resize((width, height), resample=Image.Resampling.LANCZOS)
     # test = img1.copy()
     # test.paste(img2, (center[0]-length//2, center[1]-length//2), img2)
 
@@ -32,4 +40,4 @@ def overlay(img_path, angle=0, center=(0,0)):
     
 
 if __name__ == '__main__':
-    overlay('app/static/floorplan.png', 'app/static/octa.png')
+    overlay(img_path='app/static/floorplan.png', angle=86, center=(300,300), factor=1)
