87626225aa46820be715fa31bb38f385b6a4157c
Yew Chong <83965035+yewey2@users.noreply.github.com>
2023-08-05 15:12:34 +0800

    debug development server with timing and print

--- app/events.py
+++ app/events.py
@@ -3,6 +3,8 @@ from .extensions import socketio
 from flask import request
 from app.image_overlay import overlay
 
+from timeit import default_timer as timer
+
 # request.sid => The session id of the user 
 # can use it to identify if it is the same user
 
@@ -19,5 +21,9 @@ def handle_my_event(data=dict()):
 @socketio.on("process_image")
 def handle_my_event(data):
     # negative angle to rotate acw
+    print('processing image')
+    start = timer()
     img = overlay(data.get('img'), angle=-float(data.get('angle')), center=data.get('center'))
+    end = timer()
+    print('processed image after', end-start)
     emit('display_img', {'img': img})
